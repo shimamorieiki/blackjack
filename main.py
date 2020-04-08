0fc5eff229ba0ca721a7c37098df528eaaefa590
TK paradise <paradiseOfTK@gmail.com>
2020-04-08 16:06:01 +0900

    本格開始

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print("hello")
+# ここから動くことが判明したのでBJのアプリを作ってみる
 
-print("マスター二つ目の変更です")
+import random
+
+
+class Dealer():
+    """docstring for ."""
+
+    def __init__(self):
+        self.hand = [0,0]
+
+class Player():
+    """docstring forPlayer."""
+
+    def __init__(self):
+        self.hand = [1,1]
+
+
+def deal():#で札を配る
+    return random.randint(1,13)
+
+def main():
+    dl = Dealer()
+    pl = Player()
+    print(dl.hand)
+    print(pl.hand)
+
+if __name__ == '__main__':
+    main()
