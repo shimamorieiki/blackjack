a29db0ffd799df1eed940b8b26c5a7409e5c2230
TK paradise <paradiseOfTK@gmail.com>
2020-04-08 16:29:52 +0900

    ディールに取り掛かった

--- main.py
+++ main.py
@@ -2,28 +2,39 @@
 
 import random
 
-
 class Dealer():
     """docstring for ."""
 
     def __init__(self):
-        self.hand = [0,0]
+        self.hand = []
+
+    def add_card(self):
+        self.hand.append(random.randint(1,13))
+
+    def show_cards(self):
+        print(self.hand)
 
 class Player():
     """docstring forPlayer."""
 
     def __init__(self):
-        self.hand = [1,1]
+        self.hand = []
 
+    def add_card(self):
+        self.hand.append(random.randint(1,13))
 
-def deal():#で札を配る
-    return random.randint(1,13)
+    def show_cards(self):
+        print(self.hand)
 
 def main():
     dl = Dealer()
     pl = Player()
-    print(dl.hand)
-    print(pl.hand)
+    dl.show_cards()
+    dl.add_card()
+    pl.add_card()
+    dl.show_cards()
+    pl.show_cards()
+    
 
 if __name__ == '__main__':
     main()
